1891272aff22a7f8e239e8d240c4813723a6e491
Val <v.e.reznikov99@gmail.com>
2023-11-06 13:05:07 +0200

    Upload Test

---
+++ src/tlalim/company/test/CompanyServiceImplTest.java
@@ -0,0 +1,73 @@
+package tlalim.company.test;
+
+import tlalim.company.dto.Employee;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompanyServiceImplTest {
+    private static final long ID1 = 0;
+    private static final int SALARY1 = 0;
+    private static final String DEPARTMENT1 = "Dep1";
+//    Employee empl1 = new Employee(ID1, "name1", SALARY1, DEPARTMENT1, DATE1);
+//    Employee empl2 = new Employee(ID2, "name2", SALARY2, DEPARTMENT2, DATE2);
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void hireEmployee() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void fireEmployee() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEmployee() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEmployeesByDepartment() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllEmployees() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getGetEmployessBySalary() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEmployeeByAge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void salaryDistributionByDepartments() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSalaryDistribution() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateDepartment() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateSalary() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void save() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void restore() {
+    }
+}
