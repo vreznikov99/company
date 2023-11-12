7b13a28bb92e937556bc861436ba340895ba8056
Val <v.e.reznikov99@gmail.com>
2023-11-12 23:32:44 +0200

    HomeWork 12.11.2023

--- src/tlalim/company/interviewTasks/tests/InterviewTasksTest.java
+++ src/tlalim/company/interviewTasks/tests/InterviewTasksTest.java
@@ -1,7 +1,7 @@
 package tlalim.company.interviewTasks.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static tlalim.company.interviewTasks.InterviewTasks.isAnagram;
+import static tlalim.company.interviewTasks.InterviewTasks.*;
 
 import java.util.HashMap;
 
@@ -9,7 +9,54 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class InterviewTasksTest {
+    private static final int N_NUMBERS = 100000;
+    int[] bigArray = new int[N_NUMBERS];
     @Test
+    @DisplayName("performance O[N] is Sum2 ")
+    void isSum2PerformanceTest() {
+        //isSum2 complexity O[N]
+        isSum2(bigArray, 1);
+    }
+    @Test
+    @DisplayName("performance O[N^2] is Sum2N2 ")
+    void isSum2N2PerformanceTest() {
+        //isSum2N2 complexity O[N^2]
+        isSum2N2(bigArray, 1);
+    }
+    @Test
+    void isSum2Test() {
+        int [] ar1 = {1000, 300, -200, 20, 40, -10};
+        assertTrue(isSum2(ar1, 800));//1000 + (-200) = 800
+        assertTrue(isSum2(ar1, 990));//1000 + (-10) = 990
+        assertFalse(isSum2(ar1, 50)); //no two number with the given sum
+    }
+    @Test
+    void isSum2N2Test() {
+        int [] ar1 = {1000, 300, -200, 20, 40, -10};
+        assertTrue(isSum2N2(ar1, 800));//1000 + (-200) = 800
+        assertTrue(isSum2N2(ar1, 990));//1000 + (-10) = 990
+        assertFalse(isSum2N2(ar1, 50)); //no two number with the given sum
+    }
+    @Test
+    void getMaxPositiveWithNegativeValueTest() {
+        int [] ar1 = {-1,100, 200, -50, 1, -100, 50};
+        int [] ar2 = {-1, -100, 200, -500, 10, -100, 50, 200};
+        assertEquals(100, getMaxPositiveWithNegativeValue(ar1));
+        assertEquals(-1,  getMaxPositiveWithNegativeValue(ar2));
+    }
+    @Test
+    void getMapOccurrencesTest() {
+        String[] strings = {
+                "lpm", "ab", "a", "lpm", "a", "aa", "lpm"
+        };
+        HashMap<String, Long> mapOccurrences = getMapOccurrences(strings);
+        assertEquals(3, mapOccurrences.get("lpm"));
+        assertEquals(2, mapOccurrences.get("a"));
+        assertEquals(1, mapOccurrences.get("ab"));
+        assertEquals(1, mapOccurrences.get("aa"));
+    }
+    @Test
+    @DisplayName("Anagram")
     void isAnagramTest() {
         String string = "yellow";
         assertTrue(isAnagram(string, "wolely"));
@@ -22,5 +69,6 @@ class InterviewTasksTest {
         assertFalse(isAnagram(string, "wolye"));//must be two 'l'
 
     }
+
 
 }
