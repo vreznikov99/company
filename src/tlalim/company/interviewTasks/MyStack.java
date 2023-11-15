ffcf6f3f97b80e480240f9041c62f9d670f1aefb
Val <v.e.reznikov99@gmail.com>
2023-11-15 11:17:01 +0200

    15.11.2023 HW assignment

---
+++ src/tlalim/company/interviewTasks/MyStack.java
@@ -0,0 +1,46 @@
+package tlalim.company.interviewTasks;
+
+/**
+ * All the methods implementations should be in the complexity O[1]
+ */
+public class MyStack {
+    //TODO data structure fields
+
+    /**
+     * adds a given number at top of the stack
+     * @param number
+     */
+    void push(int number){
+        //TODO
+    }
+
+    /**
+     *
+     * @return number and remove the  number from the stack top
+     * In the case the stack is empty the Exception (NoSuchElementException)
+     * must be thrown
+     */
+    int pull() {
+        //TODO
+        return -1;
+    }
+
+    /**
+     *
+     * @return true if stack is empty otherwise false
+     */
+    boolean isEmpty(){
+        //TODO
+        return false;
+    }
+
+    /**
+     *
+     * @return maximal number existing in the stack
+     * In the case the stack is empty the Exception (NoSuchElementException) must be thrown
+     */
+    int getMax(){
+        return -1;
+    }
+
+}
